088c91f83f2efdd548eed19ffea914798e1b2f13
Jianjiang Pan <panjianjiang@hotmail.com>
2026-05-06 18:55:47 +0800

    Delete emacs directory

--- emacs/lisp/pharo-smalltalk-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package
-  "pharo-smalltalk"
-  "0.1.0"
-  "Live Pharo Smalltalk bridge and tools"
-  '((emacs "29.1"))
-  :url "https://github.com/panjianjiang"
-  :keywords '("languages" "tools" "smalltalk"))
